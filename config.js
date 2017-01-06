f3a5edf2720b0e2355e7f5cc0fc81d7befab38cc
Eric Halsey <Eric Halsey>
2017-01-06 07:10:57 -0800

    Initial  nodejs-apponlytoken-rest-sample-master

---
+++ config.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Microsoft All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+// Application credentials from the Azure Management Portal.
+module.exports = {
+  clientId: 'c48aecef-f346-413c-8c76-835aa6c084ee',
+  clientSecret: '97YueAEGyzGlruCGD41cKm2lC3yP+ZT5glz/QprNJQ0=',
+  tokenEndpoint: 'https://login.windows.net/f8ac75ce-d250-407e-b8cb-e05f5b4cd913/oauth2/token'
+};
